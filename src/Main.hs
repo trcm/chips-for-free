63bbecfb0fbb1554a1469da6f13479bcaacab8da
Tom <midson.trc@gmail.com>
2019-06-07 21:30:15 +1000

    Started proper implementation

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
@@ -15,6 +16,8 @@ import           Data.Vector
 import           GHC.Word                       ( Word8
                                                 , Word16
                                                 )
+import Text.Printf
+import Data.Bits
 import           Data.Function
 
 type Rom = BS.ByteString
@@ -24,35 +27,52 @@ data Cpu = Cpu { _pc :: Int }
 data EmuState = Running | Stopped | Finished
 
 data Emu m a where
-  ReadInstruction :: Int -> Rom -> Emu m Word8
+  LoadRom :: FilePath -> Emu m Rom
+  ReadInstruction :: Int -> Rom -> Emu m Word16
 
 makeSem ''Emu
 
 data CpuEff m a where
   DecodeOpcode :: Word8 -> CpuEff m a
-  ProcessInstruction :: a -> CpuEff m a
+  -- Process opcode, return next modified state
+  ProcessInstruction :: Rom -> Cpu -> CpuEff m Cpu
 
 makeSem ''CpuEff
 
-loadRom :: IO BS.ByteString
-loadRom = BS.readFile "./roms/PONG"
+-- Input will be the filepath of the rom, output will be the buffer printed to screen
+runEmu :: (Member (Input FilePath) r, Member (Output Word16) r, Member Emu r) => Sem r ()
+runEmu = do
+  fp <- input @FilePath 
+  rom <- loadRom fp
+  let initialCpu = Cpu 0
+  is <- readInstruction 0 rom
+  output is
 
-startEmu :: (Member (Input Rom) r, Member (Input Cpu) r, Member Emu r) => Sem r ()
-startEmu = do
-  rom <- input @Rom
-  cpu <- input @Cpu
-  readInstruction (_pc cpu) rom
-  undefined
+romProvider :: Member (Lift IO) r => Sem (Emu ': r) a -> Sem r a
+romProvider = interpret $ \case
+  LoadRom fp -> sendM $ BS.readFile fp
+  ReadInstruction pc rom -> let upper = BS.index rom pc
+                                lower = BS.index rom (pc + 1)
+                            in pure $ mkWord16 upper lower
 
-runEmu :: Sem (Emu ': r ) a -> Sem r a
-runEmu = interpret $ \case
-  ReadInstruction pc rom -> do
-    pure $ BS.index rom pc
+mkWord16 :: Word8 -> Word8 -> Word16
+mkWord16 upper lower = 
+  let lo = 0x00FF .&. fromIntegral lower :: Word16
+      hi = shift (fromIntegral upper :: Word16) 8
+  in  hi .|. lo
 
--- echo forever
+runConsoleOutput :: Member (Lift IO) r => Sem (Output Word16 ': r) a -> Sem r a
+runConsoleOutput = interpret $ \case
+  Output a -> sendM . putStrLn $ printInHex a
+
+printInHex :: Word16 -> String
+printInHex a' = printf "0x%02x" a'
+
 main :: IO ()
 main = do
-  rom <- loadRom
-  startEmu & runConstInput @Rom rom & runConstInput @Cpu (Cpu 0) & runEmu & runM
-
+  runEmu
+    & runConstInput "./roms/PONG"
+    & romProvider
+    & runConsoleOutput
+    & runM
 
